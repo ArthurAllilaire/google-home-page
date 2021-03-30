893a4de4f074acc140d6332a58bde3bf0d84a419
Arthur Allilaire <arthur.allilaire@gmail.com>
2021-03-30 12:07:47 +0100

    Made footer using css grid and flexbox

--- google-home/src/App.js
+++ google-home/src/App.js
@@ -20,7 +20,7 @@ function Header() {
 function Search() {
   return(
     <div className="search-container">
-
+      <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt="Google logo" />
     </div> 
   )
 }
@@ -28,7 +28,22 @@ function Footer() {
   return(
     // I can see this being css grid with three rows - for tomorrow
     <div className="footer-container">
-
+      <div class="location-container" id="uk">United Kingdom</div>
+      <div className="footer-list">
+        <div class="left">
+          <div class="list-item">Advertising</div>
+          <div class="list-item">Business</div>
+          <div class="list-item">How Search Works</div>
+        </div>
+        <div class="center" id="carbon-center">
+          Carbon neutral since 2007
+        </div>
+        <div class="right">
+          <div class="list-item">Privacy</div>
+          <div class="list-item">Terms</div>
+          <div class="list-item">Settings</div>
+        </div>
+      </div>
     </div> 
   )
 }
